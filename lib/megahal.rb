5accfcc477307866e397fff441403b83a613b133
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2014-06-18 21:52:39 +0200

    Making tappi a bit more insane

--- lib/megahal.rb
+++ lib/megahal.rb
@@ -30,10 +30,15 @@ class MegaHal
     return if addressed_text =~ /^kumpi \w+/
     return if addressed_text =~ /^genre \w+/
 
-    reply = hal.doreply(addressed_text).force_encoding('ISO-8859-1').encode('UTF-8')
+    longreply = hal.doreply(addressed_text).force_encoding('ISO-8859-1').encode('UTF-8')
+    phrases   = longreply.split(/[\.\?\!\,]/).map(&:strip)
+    phrase1   = phrases[Random.rand(phrases.size)].split(' ')
+    phrase2   = phrases[Random.rand(phrases.size)].split(' ')
+    part1     = phrase1[0..(phrase1.size / 2)].join(' ')
+    part2     = phrase2[(phrase2.size / 2)..(phrase2.size)].join(' ')
 
     hal.learn(addressed_text.encode('ISO-8859-1'))
 
-    m.reply("#{m.user.nick}: #{reply}")
+    m.reply("#{m.user.nick}: #{part1} #{part2}.")
   end
 end
