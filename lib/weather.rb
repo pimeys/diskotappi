4758be9b30705efb1cdb6e188ff3e5295ee87e3d
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2014-07-09 11:55:24 +0200

    Nyt jumalauta

--- lib/weather.rb
+++ lib/weather.rb
@@ -19,8 +19,8 @@ class Weather
       helsinki = JSON.parse(open_uri("http://api.openweathermap.org/data/2.5/find?q=helsinki&units=metric"))
       berlin   = JSON.parse(open_uri("http://api.openweathermap.org/data/2.5/find?q=berlin&units=metric"))
 
-      temp_hki = helsinki['main']['temp'].to_f
-      temp_bln = berlin['main']['temp'].to_f
+      temp_hki = helsinki['list'].first['main']['temp'].to_f
+      temp_bln = berlin['list'].first['main']['temp'].to_f
 
       if temp_hki > temp_bln
         m.channel.notice("Helsinki #{temp_hki}ºC > Berlin #{temp_bln}ºC")
