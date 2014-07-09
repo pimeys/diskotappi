d4a4821b9eea6ab0a19e92346cbe896281ee1f2c
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2014-07-09 11:39:53 +0200

    Modification for the weather to accept no params also

--- lib/weather.rb
+++ lib/weather.rb
@@ -12,16 +12,33 @@ class Weather
     return if (m.message =~ /\A!w [a-zA-Z\-]/).nil?
 
     location = m.message.split('!w ').last
-    data     = JSON.parse(open_uri("http://api.openweathermap.org/data/2.5/find?q=#{location}&units=metric"))
 
-    return if data['list'].empty?
+    if location
+      data     = JSON.parse(open_uri("http://api.openweathermap.org/data/2.5/find?q=#{location}&units=metric"))
 
-    weather     = data['list'].first
-    place       = weather['name']
-    temp        = weather['main']['temp']
-    description = weather['weather'].first['description']
+      return if data['list'].empty?
 
-    m.channel.notice("#{place}: #{temp}ºC, #{description}")
+      weather     = data['list'].first
+      place       = weather['name']
+      temp        = weather['main']['temp']
+      description = weather['weather'].first['description']
+
+      m.channel.notice("#{place}: #{temp}ºC, #{description}")
+    else
+      m.channel.notice("Berlin > Helsinki (toimii toimii)") and return if rand(100) == 99
+
+      helsinki = JSON.parse(open_uri("http://api.openweathermap.org/data/2.5/find?q=helsinki&units=metric"))
+      berlin   = JSON.parse(open_uri("http://api.openweathermap.org/data/2.5/find?q=berlin&units=metric"))
+
+      temp_hki = helsinki['main']['temp'].to_f
+      temp_bln = berlin['main']['temp'].to_f
+
+      if temp_hki > temp_bln
+        m.channel.notice("Helsinki #{temp_hki}ºC > Berlin #{temp_bln}ºC")
+      else
+        m.channel.notice("Berlin #{temp_bln}ºC > Helsinki #{temp_hki}ºC")
+      end
+    end
   end
 
   private
