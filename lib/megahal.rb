938bc4506696da16fe6b5b35d4da194ad4cee99a
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2014-07-29 00:00:54 +0300

    Better phrases for hal

--- lib/megahal.rb
+++ lib/megahal.rb
@@ -34,11 +34,12 @@ class MegaHal
     phrases   = longreply.split(/[\.\?\!\,]/).map(&:strip)
     phrase1   = phrases[Random.rand(phrases.size)].split(' ')
     phrase2   = phrases[Random.rand(phrases.size)].split(' ')
-    part1     = phrase1[0..(phrase1.size / 2)].join(' ')
-    part2     = phrase2[(phrase2.size / 2)..(phrase2.size)].join(' ')
+    part1     = phrase1[0..(phrase1.size / 2)]
+    part2     = phrase2[(phrase2.size / 2)..(phrase2.size)]
+    reply     = part1.last == part2.first ? part1[0..-2] + part2 : part1 + part2
 
     hal.learn(addressed_text.encode('ISO-8859-1'))
 
-    m.reply("#{m.user.nick}: #{part1} #{part2}.")
+    m.reply("#{m.user.nick}: #{reply.join(' ').capitalize}.")
   end
 end
