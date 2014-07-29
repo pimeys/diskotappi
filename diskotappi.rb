75606491b8f87e7ee3a388c52eec1b965169928e
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2014-07-29 13:25:03 +0300

    Random mixtape

--- diskotappi.rb
+++ diskotappi.rb
@@ -1,16 +1,6 @@
 require 'cinch'
 
-require './lib/youtube'
-require './lib/soundcloud'
-require './lib/mixcloud'
-require './lib/spotify'
-require './lib/decision_maker'
-require './lib/genre_generator'
-require './lib/url_log'
-require './lib/megahal'
-require './lib/gfycat'
-require './lib/weather'
-require './lib/vimeo'
+Dir['./lib/*.rb'].each { |file| require file }
 
 bot = Cinch::Bot.new do
   config = YAML.load_file('./config/diskotappi.yml')
