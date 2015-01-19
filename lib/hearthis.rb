4f521fb10c7a9b8466b5e8261af0dfb52ec03964
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2015-01-19 09:04:11 +0100

    Hearthis support

---
+++ lib/hearthis.rb
@@ -0,0 +1,17 @@
+require 'cinch'
+
+require './lib/oembed_title_fetcher'
+
+class Hearthis < OembedTitleFetcher
+  include Cinch::Plugin
+
+  listen_to :channel
+
+  def allowed_hosts
+    %w(www.hearthis.at hearthis.com)
+  end
+
+  def oembed(uri)
+    "https://hearthis.at/oembed/?format=json&url=#{uri}"
+  end
+end
