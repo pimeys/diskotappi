fd1b9d22be90aa75e21f2ea2aac41fc62907bf44
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2015-02-08 13:46:05 +0100

    Quotes

---
+++ lib/quote.rb
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+require 'cinch'
+
+require './lib/bot_helper'
+require './lib/database'
+
+class Quote
+  include Cinch::Plugin
+
+  listen_to :channel
+
+  def listen(m)
+    return if (m.message =~ /\A!q/).nil?
+
+    dataset = Database.connection[:quotes]
+
+    if quote_mode(m.message) == :add
+      text = m.message.split(' add ').last
+
+      return if dataset.where(text: text).count > 0
+
+      dataset.insert(text: text)
+
+      m.reply("#{m.user.nick}, Quote added")
+    elsif quote_mode(m.message) == :named_random
+      search = m.message.split('!q ').last
+      text   = dataset.select(:text).where{ Sequel.like(:text, "%#{search}%")}.
+               order { rand{} }.first
+
+      m.reply(text[:text]) if text
+    else
+      text = dataset.select(:text).order { rand{} }.first
+
+      m.reply(text[:text]) if text
+    end
+  end
+
+  def quote_mode(message)
+    if message =~ /\A!q add (<[^>]+>) \w+/
+      :add
+    elsif message =~ /\A!q \w+/
+      :named_random
+    else
+      :random
+    end
+  end
+end
