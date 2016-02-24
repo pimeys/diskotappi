b8eb1ea11adca634d79ff979bbff9d628f00bdae
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2016-02-24 10:48:13 +0100

    Parse tweets to channel

---
+++ lib/twitter.rb
@@ -0,0 +1,28 @@
+require 'uri'
+require 'json'
+require 'cinch'
+require 'nokogiri'
+
+require './lib/oembed_title_fetcher'
+
+class Twitter < OembedTitleFetcher
+  include Cinch::Plugin
+
+  listen_to :channel
+
+  def allowed_hosts
+    %w(www.twitter.com twitter.com)
+  end
+
+  def listen(m)
+    uris = parse_uris(m)
+
+    return if uris.empty?
+
+    tweet = JSON.parse(OpenUri.("https://api.twitter.com/1/statuses/oembed.json?#{uris.first}"))["html"]
+
+    tweet_text = Nokogiri::HTML(tweet).text.strip
+
+    m.chammel.notice("#{self.class.name} :: #{tweet_text}")
+  end
+end
