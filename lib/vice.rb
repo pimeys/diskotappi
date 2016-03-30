a4bdac2a0ab70336be1bfc8c5a01b7900352fa01
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2016-03-30 16:05:02 +0200

    Vice quotes

---
+++ lib/vice.rb
@@ -0,0 +1,116 @@
+require 'cinch'
+
+require './lib/vice_dict.rb'
+
+class Vice
+  include Cinch::Plugin
+  include ViceDict
+
+  listen_to :channel
+
+  def listen(m)
+    return if (m.message =~ /\A!vice/).nil?
+
+    m.reply("VICE: #{phrases.sample}")
+  end
+
+  private
+
+  def phrases
+    [
+      HOW_WHY.sample + " " + PLACE.sample + "\'s " + OCCUPATION.sample + "s " + "are " +
+        SWAPPING.sample + " " + OBJECT1.sample + "s " + "for " + OBJECT2.sample + "s",
+
+      WE.sample + " Spent " + TIME.sample + " " + SCENARIO.sample + " with " + PLACE.sample +
+        "\'s " + OCCUPATION.sample + "s", DRUG_TAKING.sample + " " + DRUG.sample + " " +
+        SCENARIO.sample + " with " + PERSON.sample,
+
+      "A " + STATUS_RELIGION_NATIONALITY.sample + " " + OCCUPATION.sample + "\'s " +
+        GUIDE.sample + " " + PLACE.sample,
+
+      OBJECT1.sample + "s, " + OBJECT2.sample + "s and " + DRUG.sample + ": " +
+        SCENARIO.sample + " with " + PERSON.sample + " in " + PLACE.sample,
+
+      "Is the " + NATIONALITY.sample + " " + GENRE.sample + " " + MUSIC_SCENE.sample +
+        " " + CHANGING.sample + "?",
+
+      TIME.sample + " with " + PERSON.sample + " and a " + NATIONALITY.sample + " " +
+        OCCUPATION.sample,
+
+      PERSON.sample + "\'s " + FAMILY_MEMBER.sample + " is Not a " + OCCUPATION1.sample +
+        ", But He is a " + OCCUPATION2.sample,
+
+      "Do " + OCCUPATION.sample + "s Really Need " + OBJECT.sample + "s? This " +
+        NATIONALITY.sample + " " + GENRE.sample + " Fan " + ANSWER.sample,
+
+      "We\'re " + DEBATING.sample + " " + DRUG_TAKING1.sample + " " + DRUG1.sample +
+        " vs. " + DRUG_TAKING2.sample + " " + DRUG2.sample + " with " + PERSON.sample,
+
+      "Meet the " + STATUS_RELIGION_NATIONALITY.sample + " " + OCCUPATION.sample +
+        "s Who Are Putting " + GENRE.sample + " Back on the Map",
+
+      "I " + LIVED.sample + " a " + OCCUPATION.sample + " in " + PLACE.sample +
+        " and There Was No " + PERSON.sample,
+
+      DISCOVER.sample + " What " + HAPPENED.sample + " When We Were " + SCENARIO.sample +
+        " for " + TIME.sample + " in " + PLACE.sample,
+
+      "We\'re " + DRUG_TAKING.sample + " " + DRUG.sample + " with " + PLACE.sample +
+        "\'s " + OCCUPATION.sample + "s",
+
+      "Next Month, " + PERSON.sample + " Spends " + TIME.sample + " With a " +
+        OBJECT.sample + ", " + DRUG_TAKING.sample + " " + DRUG.sample,
+
+      "This is the " + STATUS_RELIGION_NATIONALITY.sample + " " + OCCUPATION.sample +
+        " Who Wants to Make " + PLACE1.sample + " the New " + PLACE2.sample,
+
+      PERSON.sample + ": " + HOW_WHY.sample + " a " + OBJECT.sample + " Couldn\'t " +
+        SAVE.sample + " My " + FAMILY_MEMBER.sample + " From Overdosing on " + DRUG.sample + " in " + PLACE.sample,
+
+      HOW_WHY.sample + " " + PERSON1.sample + " thought " + PERSON2.sample + " was a " +
+        STATUS_RELIGION.sample + " " + OCCUPATION.sample,
+
+      HOW_WHY.sample + " " + PLACE.sample + "\'s " + COOLEST.sample + " " + BAND.sample +
+        " Have Already " + GIGGED.sample + " Next Year\'s " + FESTIVAL.sample,
+
+      DRUG_TAKING.sample + " " + DRUG.sample + " at " + FESTIVAL.sample + ": " + ARTICLE_TYPE.sample,
+
+      "Meet the Writers Behind " + PLACE.sample + "\'s Version of " + TV_SHOW.sample,
+
+      HOW_WHY.sample + " " + PLACE.sample + "\'s Answer to " + TV_SHOW.sample +
+        " Is Just " + PERSON.sample + " on " + DRUG.sample,
+
+      AGE.sample + " and " + SEXUAL_ORIENTATION.sample + ": " + PLACE.sample + "\'s " +
+        OCCUPATION.sample + "s",
+
+      AMOUNT.sample + " Reasons Why " + PLACE_FESTIVAL.sample + " is the " + WORST.sample,
+
+      WE.sample + " " + PAIN.sample + " " + PERSON.sample + " and " + SOCIAL_MEDIA_ACTION.sample,
+
+      WE.sample + " " + FOUND.sample + " " + PERSON.sample + " and a " + RELIGION.sample + " " +
+        OCCUPATION.sample + " " + DOING_BAD.sample + " Each Other",
+
+      HOW_WHY.sample + " Being a " + NATIONALITY.sample + " " + SEXUAL_ORIENTATION_RELIGION.sample +
+        " Got Me a Role on " + TV_SHOW.sample,
+
+      HOW_WHY.sample + " " + PLACE.sample + "\'s Version of " + SOCIAL_NETWORK.sample +
+        " is " + FRONT.sample + " " + PAIN.sample + " " + OCCUPATION.sample + "s",
+
+      WE.sample + " " + WENT.sample + " " + PLACE_FESTIVAL.sample + " and " +
+        BOUGHT_SOLD.sample + " " + FAKE.sample + " " + DRUG.sample + " " + REASON.sample,
+
+      WE.sample + " " + FOUND.sample + " a " + SEXUAL_ORIENTATION_RELIGION.sample + " " +
+        OCCUPATION.sample + " and didn\'t " + SAVE.sample + " them from " +
+        DOING_BAD.sample + " " + PERSON.sample,
+
+      WE.sample + " " + MET.sample + " a " + HUMAN.sample + " on " +
+        SOCIAL_NETWORK.sample +  " who " + WENT.sample + " " + PLACE_FESTIVAL.sample +
+        " and " + PAIN.sample + " their " + FAMILY_MEMBER.sample,
+
+      "I " + LIVED.sample + " " + MEMBER.sample + " of " + GANG.sample,
+
+      HOW_WHY.sample + " a " + OCCUPATION.sample + " " + GROUP.sample + " are the " +
+        COOLEST.sample + " " + GENRE.sample + " " + BAND.sample + " in " + PLACE.sample,
+    ]
+  end
+end
---
+++ lib/vice_dict.rb
@@ -0,0 +1,836 @@
+module ViceDict
+  AGE = [
+    "Young",
+    "Teenage",
+    "Old",
+    "Over the Hill"
+  ]
+
+  AMOUNT = [
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "99"
+  ]
+
+  ANSWER = [
+    "Decides",
+    "Doesn\'t Have a Clue",
+    "Finds Out",
+    "Goes Undercover",
+    "Is Undecided",
+    "Knows the Answer",
+    "Sleeps His Way to the Answer",
+    "Uncovers the Truth"
+  ]
+
+  BOUGHT_SOLD = [
+    "Bought",
+    "Gave Away",
+    "Sold",
+    "Flogged",
+    "Shifted",
+    "Purchased"
+  ]
+
+  CHANGING = [
+    "Building",
+    "Dead",
+    "Defunct",
+    "Developing",
+    "Done For",
+    "Dying",
+    "Enduring",
+    "Flourishing",
+    "Gaining Credibility",
+    "Gathering Momentum",
+    "Growing",
+    "Lacking Credibility",
+    "Mushrooming",
+    "No More",
+    "Surviving"
+  ]
+
+  COOLEST = [
+    "Coolest",
+    "Most Contemporary",
+    "Edgiest",
+    "Hippest"
+  ]
+
+  DRUG_TAKING1 = [
+    "Anal Funnelling",
+    "Bombing",
+    "Dabbing",
+    "Gumming",
+    "Hoofing",
+    "Ingesting",
+    "Injecting",
+    "Mainlining"
+  ]
+
+  DRUG_TAKING2 = [
+    "Racking up",
+    "Shelving",
+    "Smoking",
+    "Sniffing",
+    "Snorting",
+    "Space Blasting",
+    "Speedballing",
+    "Swallowing",
+    "Taking"
+  ]
+
+  DRUG1 = [
+    "2C-B",
+    "Acid",
+    "Amphetamines",
+    "Angel Dust",
+    "Base",
+    "Basuco",
+    "Birdie Powder",
+    "Blowcaine",
+    "Butane",
+    "Crack Cocaine",
+    "Crystal Meth",
+    "Ecstasy",
+    "Freebase",
+    "GHB"
+  ]
+
+  DRUG2 = [
+    "Glue",
+    "Gutter Glitter",
+    "Heroin",
+    "Ketamin",
+    "Krokodil",
+    "LSD",
+    "Magic Mushrooms",
+    "Mescalin",
+    "Mephedrone",
+    "MCAT",
+    "Narcotics",
+    "Nose Candy",
+    "PCP",
+    "Rohypnol",
+    "Shrooms",
+    "Weasel Dust",
+    "Weed"
+  ]
+
+  FAKE = [
+    "Bogus",
+    "Fake",
+    "Phony",
+    "Counterfeit",
+    "Moody",
+    "Dodgy"
+  ]
+
+  FAMILY_MEMBER = [
+    "Brother",
+    "Brother-in-Law",
+    "Chef",
+    "Cleaner",
+    "Cousin",
+    "Dad",
+    "Gay Best Friend",
+    "Grandfather",
+    "Gym Buddy",
+    "Male Stripper",
+    "Neighbour",
+    "Pet Dog",
+    "Postman",
+    "Step Brother",
+    "Uncle"
+  ]
+
+  FESTIVAL = [
+    "Burning Man",
+    "Boomfest",
+    "Coachella",
+    "Creamfields",
+    "Donauinselfest",
+    "Field Day",
+    "Glastonbury",
+    "Global Gathering",
+    "Lovebox",
+    "Mawazine",
+    "Przystanek Woodstock",
+    "Rock in Rio",
+    "Sonar",
+    "SXSW",
+    "Sundance",
+    "Warehouse Project"
+  ]
+
+  GENRE = [
+    "Ambient-pop",
+    "Baile Funk",
+    "Breakcore",
+    "Britstep",
+    "Bro-step",
+    "Chillwave",
+    "Clownstep",
+    "Cumbia",
+    "Doghouse",
+    "Danger Music",
+    "Drillstep",
+    "Nu-disco",
+    "No-wave",
+    "Niche",
+    "Crunkstep",
+    "DnB-core",
+    "Dubstep",
+    "Post-EDM",
+    "Pre-IDM",
+    "Farmcore",
+    "Folk-tronica",
+    "Hardcore barbershop-tronica",
+    "Hymntronica",
+    "Post-punk",
+    "Post-trap",
+    "Psytrance",
+    "Rape-gaze",
+    "Sadcore",
+    "Seapunk",
+    "Scouse House",
+    "Shitcore",
+    "Soft Grunge",
+    "Sprock",
+    "Tumblrcore",
+    "Witchhouse"
+  ]
+
+  GIGGED = [
+    "Appeared as a Hologram at",
+    "Beatboxed at",
+    "Freestyled at",
+    "Gigged at",
+    "Rejected",
+    "Played"
+  ]
+
+  GUIDE = [
+    "Explorations in",
+    "Guide to",
+    "Tour of",
+    "Travels in",
+    "Trip Around"
+  ]
+
+  GROUP = [
+    "Cartel",
+    "Crew",
+    "Faction",
+    "Gang",
+    "Group",
+    "Subculture",
+    "Tribe"
+  ]
+
+  GANG = [
+    "The Bloods",
+    "The Crips",
+    "The Mungiki",
+    "The Russian Mob",
+    "Los Zetas",
+    "The 18th Street Gang",
+    "The Mongols",
+    "The Triads",
+    "The Texas Syndicate",
+    "The IRA",
+    "The Cosa Nostra",
+    "The Hell’s Angels",
+    "The Yakuza",
+    "The Black Guerrilla Family",
+    "Wah Ching",
+    "The Aryan Brotherhood",
+    "The Jamaican Posse",
+    "The Burger Bar Boys",
+    "The Croxteth Crew",
+    "The Peckham Boys",
+    "The Tijuana Cartel"
+  ]
+
+  WE = [
+    "I",
+    "We"
+  ]
+
+  MUSIC_SCENE = [
+    "Scene"
+  ]
+
+  NATIONALITY = [
+    "Bolivian",
+    "Bosnian",
+    "British",
+    "Brazilian",
+    "Bulgarian",
+    "Burmese",
+    "Cambodian",
+    "Canadian",
+    "Chechnyan",
+    "Chilean",
+    "Chinese",
+    "Costa Rican",
+    "Crimean",
+    "Cuban",
+    "Ghanain",
+    "Guatemalan",
+    "Hungarian",
+    "Japanese",
+    "Jordanian",
+    "Lebanese",
+    "Liberian",
+    "Lithuanian",
+    "Macedonian",
+    "Mexican",
+    "Moldovan",
+    "Mongolian",
+    "Moscovite",
+    "Nicaraguan",
+    "North Korean",
+    "Palestinian",
+    "Pakistani",
+    "Persian",
+    "Peruvian",
+    "Philipino",
+    "Polynesian",
+    "Romanian",
+    "Russian",
+    "Serbian",
+    "Scandinavian",
+    "Somali",
+    "South African",
+    "Sri Lankan",
+    "Syrian",
+    "Venezuelan",
+    "Vietnamese",
+    "Zambian",
+    "Zimbabwean"
+  ]
+
+  OBJECT1 = [
+    "Bazooka",
+    "Beer Can",
+    "Beard Trimmer",
+    "Baseball Bat",
+    "Burka",
+    "Blood Clot",
+    "Broken Bottle",
+    "Condom",
+    "Drill",
+    "Drum Kit",
+    "Fixie",
+    "Glow Stick",
+    "Helmet",
+    "Kalashnikov",
+    "Keyboard",
+    "MacBook",
+    "Lego Brick"
+  ]
+
+  OBJECT2 = [
+    "Light Sabre",
+    "Lighter",
+    "Machete",
+    "Nailgun",
+    "Neon Light",
+    "Needle",
+    "Plaster",
+    "Plastic Fork",
+    "Riot Shield",
+    "Rifle",
+    "Rocket Launcher",
+    "Rubber Duck",
+    "Samurai Sword",
+    "Synthesizer",
+    "Tampon",
+    "Thong",
+    "TV Remote"
+  ]
+
+  OCCUPATION1 = [
+    "Barista",
+    "Bouncer",
+    "Burger Flipper",
+    "Child Porn Fanatic",
+    "Creative",
+    "Death Row Inmate",
+    "Deep Sea Diver",
+    "Donkey Fluffer",
+    "Drug Addict",
+    "Drug Baron",
+    "Film Maker",
+    "Fishmonger",
+    "Graffiti Crew Member",
+    "Hacker",
+    "Hooker",
+    "Hipster",
+    "Human Trafficker",
+    "Jimmy Saville impersonator"
+  ]
+
+  OCCUPATION2 = [
+    "Media Runner",
+    "Midget Stripper",
+    "Mime Artist",
+    "Miner",
+    "Necrophiliac",
+    "Pig Farmer",
+    "Pirate",
+    "Priest",
+    "Programmer",
+    "Psychologist",
+    "Prostitute",
+    "Rapper",
+    "Rocket Scientist",
+    "Scatologist",
+    "Serial Killer",
+    "Sex Slave",
+    "Socialite",
+    "Supermodel",
+    "Steel Worker",
+    "Taxi Driver"
+  ]
+
+  PERSON1 = [
+    "Aaron Schwarz",
+    "Adolf Hitler",
+    "Angela Merkel",
+    "Barack Obama",
+    "Bashar al-Assad",
+    "Ben Affleck",
+    "Beyonce",
+    "Bill Clinton",
+    "Britney Spears",
+    "Brian Blessed",
+    "Calvin Harris",
+    "Cara Delevingne",
+    "Chairman Mao",
+    "Charles Taylor",
+    "Che Guevara",
+    "Colonel Qaddafi",
+    "Condoleezza Rice",
+    "Courtney Love",
+    "Chris Brown",
+    "Chuck D",
+    "Dr. Dre",
+    "Daniel Radcliffe",
+    "David Bowie",
+    "David Letterman",
+    "Dick Cheney",
+    "Drew Barrymore",
+    "Fidel Castro",
+    "Gary Glitter",
+    "George W. Bush",
+    "Harry Styles",
+    "Heisenberg",
+    "Hilary Clinton",
+    "Hugo Chavez",
+    "James Franco",
+    "Jay-Z",
+    "Jason Biggs",
+    "John Belushi",
+    "John Lennon",
+    "Justin Bieber",
+    "Kanye West"
+  ]
+
+  PERSON2 = [
+    "Kanye West",
+    "Kelsey Grammar",
+    "Kim Dotcom",
+    "Kim Jong Un",
+    "Lenin",
+    "Lil Wayne",
+    "Lindsay Lohan",
+    "Mahmoud Ahmadinejad",
+    "Marilyn Monroe",
+    "Matthew McConaughey",
+    "Method Man",
+    "Michael Barrymore",
+    "Michael Jackson",
+    "Miley Cyrus",
+    "Mike Tyson",
+    "Mitt Romney",
+    "Morgan Freeman",
+    "Mussolini",
+    "Nigel Farage",
+    "Nigella Lawson",
+    "Nick Griffin",
+    "Notorious B.I.G.",
+    "Paris Hilton",
+    "Paul McCartney",
+    "Peaches Geldof",
+    "Phil Collins",
+    "Pol Pot",
+    "Psy",
+    "Puff Daddy",
+    "R Kelly",
+    "Ricky Gervais",
+    "Richard Dawkins",
+    "Rihanna",
+    "Robert Downey Jr.",
+    "Robert Mugabe",
+    "Robin Thicke",
+    "Rolf Harris",
+    "Ryan Gosling",
+    "Saddam Hussein",
+    "Satan",
+    "Seth Rogen",
+    "Shakespeare",
+    "Snoop Dogg",
+    "Soldier Boy",
+    "Stanley Kubrick",
+    "Stalin",
+    "Tara Reid",
+    "Tupac",
+    "Vladimir Putin"
+  ]
+
+  PLACE1 = [
+    "Bangkok",
+    "Beirut",
+    "Belgrade",
+    "Berlin",
+    "Borneo",
+    "Brooklyn",
+    "Chile",
+    "Croydon",
+    "Cuba",
+    "Denmark",
+    "Egypt",
+    "Gaza",
+    "Guadalahara",
+    "Hackney",
+    "Hanoi",
+    "Helsinki",
+    "Iran",
+    "Iraq",
+    "Jerusalem"
+  ]
+
+  PLACE2 = [
+    "Johannesburg",
+    "Kiev",
+    "Lebanon",
+    "Montenegro",
+    "Moscow",
+    "NY",
+    "North Korea",
+    "Osaka",
+    "Pakistan",
+    "Penang",
+    "Russia",
+    "Santiago",
+    "Seoul",
+    "Serbia",
+    "Sudan",
+    "Syria",
+    "Timbuktu",
+    "Zagreb",
+    "Zurich",
+    "Houston",
+    "Ukraine",
+    "Congo",
+    "Sao Paolo",
+    "Williamsburg"
+  ]
+
+  PLACE = PLACE1 + PLACE2
+
+  SEXUAL_ORIENTATION = [
+    "Gay",
+    "Homosexual",
+    "Lesbian",
+    "Feminist",
+    "Queer"
+  ]
+
+  SCENARIO = [
+    "At College",
+    "At Work",
+    "Conducting Sacrifices",
+    "In a Coma",
+    "In Bed",
+    "In Rehab",
+    "In School",
+    "At a Sex Party",
+    "In the Studio",
+    "On Location",
+    "On the Streets",
+    "On Vacation",
+    "Playing Poker",
+    "Playing Russian Roulette",
+    "Undercover",
+    "Underground"
+  ]
+
+  STATUSES = [
+    "Bi-curious",
+    "Down Syndrome",
+    "Thalidomide",
+    "Paraplegic",
+    "Transgender"
+  ]
+
+  SWAPPING = [
+    "Exchanging",
+    "Substituting",
+    "Swapping",
+    "Switching",
+    "Trading"
+  ]
+
+  TIME = [
+    "24 Hours",
+    "48 Hours",
+    "5 Minutes",
+    "72 Hours",
+    "A Fucking Eternity",
+    "A Month",
+    "A Week",
+    "A Year",
+    "A Decade",
+    "Channukah",
+    "Christmas",
+    "Easter",
+    "Eid",
+    "Halloween",
+    "January",
+    "Passover",
+    "Fucking Ages",
+    "Way Too Long"
+  ]
+
+  TV_SHOW = [
+    "Archer",
+    "Arrested Development",
+    "Avatar",
+    "Batman",
+    "Breaking Bad",
+    "CSI",
+    "Dexter",
+    "Doctor Who",
+    "Fawlty Towers",
+    "Friends",
+    "Game of Thrones",
+    "Harry Potter",
+    "House of Cards",
+    "Lost",
+    "Mad Men",
+    "M.A.S.H.",
+    "Seinfeld",
+    "Star Trek",
+    "Star Wars",
+    "The Killing",
+    "The Sopranos",
+    "The Tonight Show",
+    "The X Files",
+    "The Wire",
+    "Top Gear",
+    "True Detective",
+    "Twin Peaks",
+    "T.O.W.I.E.",
+    "Catfish"
+  ]
+
+  PAIN = [
+    "Killed",
+    "Tortured",
+    "Molested",
+    "Executed",
+    "Strangled",
+    "Murdered",
+    "Whored Out",
+    "Violated",
+    "Defiled",
+    "Abused",
+    "Assaulted"
+  ]
+
+  RELIGION = [
+    "Christian",
+    "Hindu",
+    "Jewish",
+    "Muslim",
+    "Scientologist",
+    "Buddhist",
+    "Taoist",
+    "Sikh",
+    "Pagan",
+    "Kemetist"
+  ]
+
+  SOCIAL_MEDIA_ACTION = [
+    "Posted it on Facebook",
+    "Live Tweeted the Whole Thing",
+    "Instagrammed the Shit out of it",
+    "Built an App for it",
+    "Made a Hilarious Vine of it",
+    "Started an AMA on Reddit",
+    "Created an Account for it on Tinder",
+    "Sent Out a Bunch of Snapchats",
+    "Slapped it on Youtube"
+  ]
+
+  SOCIAL_NETWORK = [
+    "Facebook",
+    "Twitter",
+    "Instragram",
+    "Secret",
+    "Grindr",
+    "Vine",
+    "Reddit",
+    "Tinder",
+    "Snapchat",
+    "MySpace",
+    "AirBnB"
+  ]
+
+  DOING_BAD = [
+    "Fellating",
+    "Having Sex with",
+    "Kissing"
+  ]
+
+  FOUND = [
+    "Caught",
+    "Documented",
+    "Filmed",
+    "Found"
+  ]
+
+  WORST = [
+    "Most Backward Place We\'ve Ever Been",
+    "Seediest Place in the World",
+    "Most Dangerous Place on Earth"
+  ]
+
+  HOW_WHY = [
+    "How",
+    "Why"
+  ]
+
+  ARTICLE_TYPE = [
+    "The Definitive Guide",
+    "A Documentary",
+    "We Go Deep Undercover"
+  ]
+
+  FRONT = [
+    "Just a Front for",
+    "Full of",
+    "Packed with",
+    "A Facade For"
+  ]
+
+  BAND = [
+    "Band"
+  ]
+
+  LIVED = [
+    "Lived as",
+    "Began Life as",
+    "Masqueraded as",
+    "Pretended to be"
+  ]
+
+  REASON = [
+    "For No Reason At All",
+    "For Shits and Giggles",
+    "For A laugh",
+    "As a Wind Up",
+    "For Fun",
+    "Because That\'s Journalism",
+    "To Write This Article",
+    "As a Joke"
+  ]
+
+  SAVE = [
+    "Save",
+    "Stop",
+    "Prevent"
+  ]
+
+  DEBATING = [
+    "Debating",
+    "Deliberating",
+    "Considering"
+  ]
+
+  DISCOVER = [
+    "Discover",
+    "Find Out",
+    "See"
+  ]
+
+  WENT = [
+    "Went To",
+    "Flew To",
+    "Travelled To",
+    "Hitchhiked To"
+  ]
+
+  HAPPENED = [
+    "Happened",
+    "Went On",
+    "Went Down"
+  ]
+
+  HUMAN = [
+    "Man",
+    "Woman",
+    "Child",
+    "Teenager",
+    "Girl",
+    "Boy",
+    "Baby"
+  ]
+
+  MET = [
+    "Met",
+    "Stumbled Across",
+    "Found",
+    "Ignored"
+  ]
+
+  MEMBER = [
+    "A Member",
+    "A Representative",
+    "A Foe",
+    "An Ememy",
+    "A Whistleblower"
+  ]
+
+  STATUS_RELIGION = STATUSES + RELIGION
+  STATUS_RELIGION_NATIONALITY = STATUS_RELIGION + NATIONALITY
+  SEXUAL_ORIENTATION_RELIGION = SEXUAL_ORIENTATION + RELIGION
+  PLACE_FESTIVAL = PLACE1 + PLACE2 + FESTIVAL
+  OCCUPATION = OCCUPATION1 + OCCUPATION2
+  DRUG_TAKING = DRUG_TAKING1 + DRUG_TAKING2
+  DRUG = DRUG1 + DRUG2
+  PERSON = PERSON1 + PERSON2
+  OBJECT = OBJECT1 + OBJECT2
+end
