8800838e9caec0c0c36753c5502312c5ce2df520
FistOfTheNorthStar <k.kulvik@gmail.com>
2017-12-13 14:29:24 +0000

    Updated README for air quality

--- lib/weather.rb
+++ lib/weather.rb
@@ -59,6 +59,7 @@ class Weather
       return if !air_results.air_quality(temp)
 
       m.channel.notice(air_results.irc_string)
+    
     else
       weather = fetch_weather(location)
 
@@ -83,9 +84,6 @@ class Weather
   def fetch_sauna
     JSON.parse(OpenUri.("http://sompasauna.fi/lampotila/index.php/on/saunassa?format=json"))
   end
-
-  def fetch_air_quality(location)
-	
 
   def fetch_kertsi
     JSON.parse(OpenUri.("https://api.particle.io/v1/devices/380036001047343432313031/diskotappi?access_token=#{config['kertsi_key']}"))
